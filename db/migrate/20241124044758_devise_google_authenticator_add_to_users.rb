44933cccd9b1448d84f572ed4a77960074213f66
Ginger-Stone <doreenjeptoo@gmail.com>
2024-11-24 09:13:01 +0300

    first commit

---
+++ db/migrate/20241124044758_devise_google_authenticator_add_to_users.rb
@@ -0,0 +1,18 @@
+class DeviseGoogleAuthenticatorAddToUsers < ActiveRecord::Migration[6.1]
+  def self.up
+    change_table :users do |t|
+      t.string  :gauth_secret
+      t.string  :gauth_enabled, :default => "f"
+      t.string  :gauth_tmp
+      t.datetime  :gauth_tmp_datetime
+      t.datetime :remember_gauth_token_set_at
+      t.datetime :remember_gauth_token_expires_at
+    end
+  end
+
+  def self.down
+    change_table :users do |t|
+      t.remove :gauth_secret, :gauth_enabled, :gauth_tmp, :gauth_tmp_datetime, :remember_gauth_token_set_at, :remember_gauth_token_expires_at
+    end
+  end
+end
